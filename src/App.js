a2cf77c685468f6ad13a05378f67ea26a94ced3e
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-12 08:12:17 +1100

    add basic layout with html/css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 function App() {
 	return (
-		<div className="App">
-			<p>Hello World</p>
+		<div id="drum-machine" className="container">
+			<div id="display">Display</div>
+			<div id="drum-pads">
+				<button id="closed-hat" type="button" className="drum-pad">
+					Q
+				</button>
+				<button id="open-hat" type="button" className="drum-pad">
+					W
+				</button>
+				<button id="kick" type="button" className="drum-pad">
+					E
+				</button>
+				<button id="snare1" type="button" className="drum-pad">
+					A
+				</button>
+				<button id="snare2" type="button" className="drum-pad">
+					S
+				</button>
+				<button id="clap" type="button" className="drum-pad">
+					D
+				</button>
+				<button id="tom" type="button" className="drum-pad">
+					Z
+				</button>
+				<button id="low-tom" type="button" className="drum-pad">
+					X
+				</button>
+				<button id="clave" type="button" className="drum-pad">
+					C
+				</button>
+			</div>
 		</div>
 	);
 }
