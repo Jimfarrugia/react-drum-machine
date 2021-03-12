7f478c900b1a5775dc89ddd5db2aea847cee6cab
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-03-12 11:02:38 +1100

    create a seperate file to store clips data

---
+++ src/clips.js
@@ -0,0 +1,49 @@
+const clips = [
+	{
+		trigger: "Q",
+		name: "Closed Hat",
+		filename: "closed-hat",
+	},
+	{
+		trigger: "W",
+		name: "Open Hat",
+		filename: "open-hat",
+	},
+	{
+		trigger: "E",
+		name: "Kick",
+		filename: "kick",
+	},
+	{
+		trigger: "A",
+		name: "Snare 1",
+		filename: "snare1",
+	},
+	{
+		trigger: "S",
+		name: "Snare 2",
+		filename: "snare2",
+	},
+	{
+		trigger: "D",
+		name: "Clap",
+		filename: "clap",
+	},
+	{
+		trigger: "Z",
+		name: "Tom",
+		filename: "tom",
+	},
+	{
+		trigger: "X",
+		name: "Low Tom",
+		filename: "low-tom",
+	},
+	{
+		trigger: "C",
+		name: "Clave",
+		filename: "clave",
+	},
+];
+
+export default clips;
